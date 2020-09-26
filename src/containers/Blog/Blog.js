9665eafe31ce7a3e84578f39f9fa0af45a92bcb8
Shyamal Shah <shyamalsha@gmail.com>
2020-09-26 15:28:33 -0400

    Updated with axios ajax call

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,16 +3,36 @@ import React, { Component } from 'react';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
+import axios from 'axios';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts:[]
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then((response) =>{
+            const posts = response.data.slice(0,4)
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author:'Shyamal'
+                }
+            });
+            this.setState({
+                posts:updatedPosts
+            });
+        });
+    }
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title} author={post.author}></Post>
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                {posts}
                 </section>
                 <section>
                     <FullPost />
