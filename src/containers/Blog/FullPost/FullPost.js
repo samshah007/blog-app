6df4b56b0f66b2623e956a7705df1a7886bcf4bd
Shyamal Shah <shyamalsha@gmail.com>
2020-10-05 22:14:12 -0400

    Route Param example

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -6,10 +6,11 @@ class FullPost extends Component {
     state = {
         loadedPost:null
     }
-    componentDidUpdate(){        
-       if(this.props.id){
-           if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)){
-                axios.get('/posts/' + this.props.id)
+    componentDidMount(){
+        console.log(this.props)        
+       if(this.props.match.params.id){
+           if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.match.params.id)){
+                axios.get('/posts/' + this.props.match.params.id)
                     .then((response) =>{
                         this.setState({loadedPost:response.data})
                 })                
@@ -24,7 +25,7 @@ class FullPost extends Component {
     }
     render () {
         let post = <p style={{textAlign:'center'}}>Please select a Post!</p>;
-        if(this.props.id){
+        if(this.props.match.params.id){
             post = <p style={{textAlign:'center'}}>Loading....</p>;
         }
         if(this.state.loadedPost){
